29284a330a17d0aeede5fdf7a70e88789e6b4129
nga47 <hathithuynga472002@gmail.com>
2026-05-05 21:41:55 +0700

    Solve Group Anagrams

---
+++ arrays_hashing/group_anagrams.py
@@ -0,0 +1,14 @@
+# Problem: Group Anagrams
+# Technique: HashMap + Sorting
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        mp = defaultdict(list)
+        
+        for s in strs:
+            key = "".join(sorted(s))
+            mp[key].append(s)
+        
+        return list(mp.values())
